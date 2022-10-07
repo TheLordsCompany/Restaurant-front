a83f5c95766beb0126190ab839f44bf71484114e
khalil kraiem <khalilmedkraiem@gmail>
2022-10-07 20:08:15 +0100

    animation carousel

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -3,9 +3,9 @@ import { Carousel } from "react-bootstrap";
 
 let array=[
   "https://res.cloudinary.com/dova8kprm/image/upload/v1665144632/ir3jtmcoonw2ce0omlzg.jpg?fbclid=IwAR1XFo29oTwQCjdg5hqXN2rvGiIoGjHQGkdtwoCufzFm7szNxb5_G8CENhQ",
-  // "https://res.cloudinary.com/dova8kprm/image/upload/v1665141708/sff7ytcwvstop6w59net.jpg?fbclid=IwAR0flM-wG-hyXzRttotY3CCNbDza57NQ2A57RCioZ90g0n857s5yfJ4MXQ4",
-  // "https://res.cloudinary.com/dova8kprm/image/upload/v1665141855/wvafjjvfy8th7dpwgyli.jpg?fbclid=IwAR3mTFbMuEfk_qtyoSjI0IEZPc4SGHUx_niX63RU-RSngtu27tZ4QT0kIPM",
-  // "https://res.cloudinary.com/dova8kprm/image/upload/v1665141971/jnzvt9sqsgvdanngmig8.jpg?fbclid=IwAR1p8lMz5kbS_BXjE3vhhvYpHPIER1GS-xiK1uMKCqFCRKOK0xlrwHSuFrc"
+  "https://res.cloudinary.com/dova8kprm/image/upload/v1665141708/sff7ytcwvstop6w59net.jpg?fbclid=IwAR0flM-wG-hyXzRttotY3CCNbDza57NQ2A57RCioZ90g0n857s5yfJ4MXQ4",
+  "https://res.cloudinary.com/dova8kprm/image/upload/v1665141855/wvafjjvfy8th7dpwgyli.jpg?fbclid=IwAR3mTFbMuEfk_qtyoSjI0IEZPc4SGHUx_niX63RU-RSngtu27tZ4QT0kIPM",
+  "https://res.cloudinary.com/dova8kprm/image/upload/v1665141971/jnzvt9sqsgvdanngmig8.jpg?fbclid=IwAR1p8lMz5kbS_BXjE3vhhvYpHPIER1GS-xiK1uMKCqFCRKOK0xlrwHSuFrc"
 ]
 function Home() {
   useEffect(()=>{
@@ -17,7 +17,7 @@ function Home() {
         pause={false}
         indicators={false}
         controls={false}
-        // activeIndex={element}
+        interval={4000}
       >
         {array.map((elem,i)=>(
 
