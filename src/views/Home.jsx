6299ef09e9702f15d196317ff926291beae0a3f5
khalil kraiem <khalilmedkraiem@gmail>
2022-10-09 11:22:58 +0100

    carousel with new pic

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,16 +1,28 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Carousel } from "react-bootstrap";
+import pic1 from '../assets/images/1.jpg'
+import pic2 from '../assets/images/3.jpg'
+import pic3 from '../assets/images/27.jpg'
+import pic4 from '../assets/images/29.jpg'
+import pic5 from '../assets/images/32.jpg'
 
-let array=[
-  "https://res.cloudinary.com/dova8kprm/image/upload/v1665144632/ir3jtmcoonw2ce0omlzg.jpg?fbclid=IwAR1XFo29oTwQCjdg5hqXN2rvGiIoGjHQGkdtwoCufzFm7szNxb5_G8CENhQ",
-  "https://res.cloudinary.com/dova8kprm/image/upload/v1665141708/sff7ytcwvstop6w59net.jpg?fbclid=IwAR0flM-wG-hyXzRttotY3CCNbDza57NQ2A57RCioZ90g0n857s5yfJ4MXQ4",
-  "https://res.cloudinary.com/dova8kprm/image/upload/v1665141855/wvafjjvfy8th7dpwgyli.jpg?fbclid=IwAR3mTFbMuEfk_qtyoSjI0IEZPc4SGHUx_niX63RU-RSngtu27tZ4QT0kIPM",
-  "https://res.cloudinary.com/dova8kprm/image/upload/v1665141971/jnzvt9sqsgvdanngmig8.jpg?fbclid=IwAR1p8lMz5kbS_BXjE3vhhvYpHPIER1GS-xiK1uMKCqFCRKOK0xlrwHSuFrc"
-]
+
+
+let array = [
+  // "https://res.cloudinary.com/dova8kprm/image/upload/v1665144632/ir3jtmcoonw2ce0omlzg.jpg?fbclid=IwAR1XFo29oTwQCjdg5hqXN2rvGiIoGjHQGkdtwoCufzFm7szNxb5_G8CENhQ",
+  // "https://res.cloudinary.com/dova8kprm/image/upload/v1665141708/sff7ytcwvstop6w59net.jpg?fbclid=IwAR0flM-wG-hyXzRttotY3CCNbDza57NQ2A57RCioZ90g0n857s5yfJ4MXQ4",
+  // "https://res.cloudinary.com/dova8kprm/image/upload/v1665141855/wvafjjvfy8th7dpwgyli.jpg?fbclid=IwAR3mTFbMuEfk_qtyoSjI0IEZPc4SGHUx_niX63RU-RSngtu27tZ4QT0kIPM",
+  // "https://res.cloudinary.com/dova8kprm/image/upload/v1665141971/jnzvt9sqsgvdanngmig8.jpg?fbclid=IwAR1p8lMz5kbS_BXjE3vhhvYpHPIER1GS-xiK1uMKCqFCRKOK0xlrwHSuFrc",
+pic1,pic2,pic3,pic4,pic5
+
+];
 function Home() {
-  useEffect(()=>{
+  const [value, setValue] = useState(0);
+  // const [bool, setBool] = useState(false);
+  useEffect(() => {
     window.scrollTo(0, 0);
-  },[])
+  }, []);
+  console.log(value);
   return (
     <div>
       <Carousel
@@ -18,14 +30,29 @@ function Home() {
         indicators={false}
         controls={false}
         interval={4000}
+        
       >
-        {array.map((elem,i)=>(
-
-        <Carousel.Item key={i}>
-          <div className="homeCarousel" style={{backgroundImage:`url(${elem})`}}></div>
-        </Carousel.Item>
-        ))}
-        </Carousel>
+        {array.map((elem, i) => {
+          
+          return (
+            <Carousel.Item key={i}>
+              <div
+                className="homeCarousel"
+                id={`carousel${i}`}
+                style={{ backgroundImage: `url(${elem})` }}
+                
+              >
+                <img src={elem} onLoad={()=>setValue(i+1)} style={{height:0,width:0}} />
+              </div>
+            </Carousel.Item>
+          );
+        })}
+      </Carousel>
+      {value < 4 && (
+        <div className="loading center">
+          <h1>Loading</h1>
+        </div>
+      )}
     </div>
   );
 }
